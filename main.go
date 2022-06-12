1ba30010437454447907e9baaa09041c9ce65f36
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-06-12 18:58:56 +0200

    main.go: implement optimizer for meeting slots
    Implementation of algorithm to find a solution to the problem of
    finding the minimal number of slots for a set of conflicting meetings.

---
+++ main.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type adjacencyMat [][]uint8
+
+type meeting struct {
+	index      int
+	title      string
+	assistants []string
+	// Slot currently assigned
+	slot int
+}
+
+// slot contains all meetings happening at the same time
+type slot struct {
+	meetings []meeting
+}
+
+// Input must be in the form of a csv file, with the first column being
+// the meeting title and the second the list of assistants.
+// Output will be a csv file with two columns, one with the slot number
+// and the second with the simultaneous meetings happening in that slot.
+// Steps:
+// 1. Read meetings from input
+// 2. Calculate adjacency matrix
+// 3. Create one slot per meeting
+// 4. Apply removal strategy iteratively until the number of slots
+//    does not decrease anymore
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Printf("Usage: %s <csv_file_in> <csv_file_out>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	csvFile, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Printf("Cannot open %s: %v\n", os.Args[1], err)
+		os.Exit(1)
+	}
+	defer csvFile.Close()
+
+	csvReader := csv.NewReader(csvFile)
+	csvData, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Printf("Error reading csv data from %s: %v\n", os.Args[1], err)
+		os.Exit(1)
+	}
+	meetings, err := createMeetingsFromCSVData(csvData)
+	if err != nil {
+		fmt.Printf("Error reading csv meeting data from %s: %v\n", os.Args[1], err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Running the algorithm...")
+	slots, err := findMinimalSlots(meetings)
+
+	outFile, err := os.Create(os.Args[2])
+	if err != nil {
+		fmt.Printf("Cannot open %s for writing: %v\n", os.Args[1], err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+	csvWriter := csv.NewWriter(outFile)
+	for sl_i, sl := range slots {
+		for _, mt := range sl.meetings {
+			assistants := ""
+			for i := 0; i < len(mt.assistants)-1; i++ {
+				assistants += mt.assistants[i]
+				assistants += ", "
+			}
+			assistants += mt.assistants[len(mt.assistants)-1]
+			csvWriter.Write([]string{fmt.Sprintf("slot %d", sl_i),
+				mt.title,
+				assistants})
+		}
+	}
+	csvWriter.Flush()
+	fmt.Println("Finished")
+}
+
+func createMeetingsFromCSVData(csvData [][]string) ([]meeting, error) {
+	meetings := make([]meeting, len(csvData))
+	for i, line := range csvData {
+		if len(line) != 2 {
+			return nil, fmt.Errorf("wrong number of fields in line %d", i)
+		}
+		mt := meeting{index: i, title: line[0]}
+		assistants := strings.Split(line[1], ",")
+		for _, person := range assistants {
+			mt.assistants = append(mt.assistants, strings.TrimSpace(person))
+		}
+		meetings[i] = mt
+	}
+
+	return meetings, nil
+}
+
+func findMinimalSlots(meetings []meeting) ([]slot, error) {
+	adj := calcAdjacencyMatrix(meetings)
+
+	// Initially, one slot per meeting
+	slots := make([]slot, len(meetings))
+	for i := range slots {
+		slots[i].meetings = append(slots[i].meetings, meetings[i])
+		meetings[i].index = i
+		meetings[i].slot = i
+	}
+
+	// Iterated until there is no slot consolidation
+	for removed := true; removed == true; removed = removeSlotsIteration(adj, &slots) {
+	}
+
+	// Remove emptied slots
+	var consolidatedSlots []slot
+	for _, sl := range slots {
+		if len(sl.meetings) != 0 {
+			consolidatedSlots = append(consolidatedSlots, sl)
+		}
+	}
+
+	return consolidatedSlots, nil
+}
+
+// Implements the removal strategy, one iteration across current slots.
+// Returns true if at least one slot has been consolidated.
+func removeSlotsIteration(adj adjacencyMat, slots *[]slot) (removed bool) {
+	for s_i, sl := range *slots {
+		// If no meetings, the slot was already removed
+		if len(sl.meetings) == 0 {
+			continue
+		}
+		// For each meeting in the slot, check if it can be moved to another slot
+		compatibleSlots := make([]int, len(sl.meetings))
+		allCompatible := true
+		for m_i, m := range sl.meetings {
+			compatibleSlots[m_i] = -1
+			for s_j, otherSl := range *slots {
+				// Ignore if same slot or if removed slot
+				if s_i == s_j || len(otherSl.meetings) == 0 {
+					continue
+				}
+				if slotCompatibleWithMeeting(otherSl, m, adj) {
+					compatibleSlots[m_i] = s_j
+					break
+				}
+			}
+			if compatibleSlots[m_i] == -1 {
+				allCompatible = false
+				break
+			}
+		}
+		// If we have found compatible slots for all meetings, transfer
+		// the meetings and remove the slot.
+		if allCompatible {
+			for m_i, m := range sl.meetings {
+				compatSl := compatibleSlots[m_i]
+				(*slots)[compatSl].meetings =
+					append((*slots)[compatSl].meetings, m)
+			}
+			(*slots)[s_i] = slot{}
+			removed = true
+		}
+	}
+
+	return removed
+}
+
+// Will be compatible if all meetings already in the slot are adjacent to m
+func slotCompatibleWithMeeting(sl slot, m meeting, adj adjacencyMat) bool {
+	for _, slotMeeting := range sl.meetings {
+		if adj[slotMeeting.index][m.index] != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+// calcAdjacencyMatrix finds out the adjacency matrix for a set of
+// meetings, that is, matrix values are 1 if row/column meetings are
+// compatible, 0 otherwise.
+func calcAdjacencyMatrix(meetings []meeting) adjacencyMat {
+	numMeets := len(meetings)
+	adj := make([][]uint8, numMeets)
+	for i := range adj {
+		adj[i] = make([]uint8, numMeets)
+	}
+
+	for m_i := range meetings {
+		for m_j := m_i + 1; m_j < numMeets; m_j++ {
+			if compatibleMeetings(&meetings[m_i], &meetings[m_j]) {
+				adj[m_i][m_j] = 1
+				adj[m_j][m_i] = 1
+			}
+		}
+	}
+	return adj
+}
+
+// compatibleMeetings returns true if two meetings are compatible,
+// that is, will return true if there are no coincident assistants
+// between the two meetings, otherwise it will return false.
+func compatibleMeetings(m1, m2 *meeting) bool {
+	for _, p1 := range m1.assistants {
+		for _, p2 := range m2.assistants {
+			if p1 == p2 {
+				return false
+			}
+		}
+	}
+	return true
+}
---
+++ main_test.go
@@ -0,0 +1,223 @@
+// go test *.go
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestCompatibleMeetings(t *testing.T) {
+	compatibleTests := []struct{ m1, m2 *meeting }{
+		{
+			&meeting{index: 0, title: "Meeting 0", assistants: []string{"a", "b"}},
+			&meeting{index: 1, title: "Meeting 1", assistants: []string{"c", "d"}},
+		},
+		{
+			&meeting{index: 0, title: "Meeting 0", assistants: []string{"a"}},
+			&meeting{index: 1, title: "Meeting 1", assistants: []string{"b", "c", "d"}},
+		},
+	}
+	for _, mt := range compatibleTests {
+		if !compatibleMeetings(mt.m1, mt.m2) {
+			t.Error(mt.m1, "incompatible with", mt.m2, "; expected compatible")
+		}
+	}
+
+	incompatibleTests := []struct{ m1, m2 *meeting }{
+		{
+			&meeting{index: 0, title: "Meeting 0", assistants: []string{"a", "b"}},
+			&meeting{index: 1, title: "Meeting 1", assistants: []string{"c", "a"}},
+		},
+		{
+			&meeting{index: 0, title: "Meeting 0", assistants: []string{"a"}},
+			&meeting{index: 1, title: "Meeting 1", assistants: []string{"b", "c", "a"}},
+		},
+	}
+	for _, mt := range incompatibleTests {
+		if compatibleMeetings(mt.m1, mt.m2) {
+			t.Error(mt.m1, "compatible with", mt.m2, "; expected incompatible")
+		}
+	}
+}
+
+func TestCalcAdjacencyMatrix(t *testing.T) {
+	mts := []meeting{
+		{index: 0, title: "Meeting 0", assistants: []string{"a", "b"}},
+		{index: 1, title: "Meeting 1", assistants: []string{"c", "d"}},
+		{index: 2, title: "Meeting 2", assistants: []string{"e", "a"}},
+		{index: 3, title: "Meeting 3", assistants: []string{"e", "d"}},
+	}
+	expMat := [][]uint8{
+		{0, 1, 0, 1},
+		{1, 0, 1, 0},
+		{0, 1, 0, 0},
+		{1, 0, 0, 0},
+	}
+	adjMat := calcAdjacencyMatrix(mts)
+	if len(expMat) != len(adjMat) {
+		t.Error(expMat, "different size from", adjMat, "; expected equal")
+	}
+	for i := range expMat[0] {
+		if len(expMat[i]) != len(adjMat[i]) {
+			t.Error(expMat, "different size from", adjMat, "; expected equal")
+		}
+		for j := range expMat[0] {
+			if expMat[i][j] != adjMat[i][j] {
+				t.Error(expMat, "different from", adjMat, "; expected equal")
+			}
+		}
+	}
+}
+
+func TestSlotCompatibleWithMeeting(t *testing.T) {
+	m0 := meeting{index: 0, title: "Meeting 0", assistants: []string{"a", "b"}}
+	m1 := meeting{index: 1, title: "Meeting 1", assistants: []string{"c", "d"}}
+	m2 := meeting{index: 2, title: "Meeting 2", assistants: []string{"e", "g"}}
+	m3 := meeting{index: 3, title: "Meeting 3", assistants: []string{"e", "h"}}
+	mts := []meeting{m0, m1, m2, m3}
+	adjMat := calcAdjacencyMatrix(mts)
+	var sl slot
+	sl = slot{meetings: []meeting{m0, m1}}
+	if !slotCompatibleWithMeeting(sl, m2, adjMat) {
+		t.Error(sl, "incompatible with", m2, "; expected compatible")
+	}
+	if !slotCompatibleWithMeeting(sl, m3, adjMat) {
+		t.Error(sl, "incompatible with", m3, "; expected compatible")
+	}
+
+	sl = slot{meetings: []meeting{m0, m1, m2}}
+	if slotCompatibleWithMeeting(sl, m3, adjMat) {
+		t.Error(sl, "compatible with", m3, "; expected incompatible")
+	}
+}
+
+func TestRemoveSlotsIteration1(t *testing.T) {
+	m0 := meeting{index: 0, title: "Meeting 0", assistants: []string{"a", "b"}}
+	m1 := meeting{index: 1, title: "Meeting 1", assistants: []string{"c", "d"}}
+	m2 := meeting{index: 2, title: "Meeting 2", assistants: []string{"e", "g"}}
+	m3 := meeting{index: 3, title: "Meeting 3", assistants: []string{"e", "h"}}
+	mts := []meeting{m0, m1, m2, m3}
+	adjMat := calcAdjacencyMatrix(mts)
+	slots := make([]slot, len(mts))
+	for m_i, m := range mts {
+		slots[m_i] = slot{meetings: []meeting{m}}
+	}
+
+	i := 0
+	for slotsRemoved := true; slotsRemoved; i++ {
+		slotsRemoved = removeSlotsIteration(adjMat, &slots)
+	}
+	if expIt := 2; i != expIt {
+		t.Error(i, "iterations: expected", expIt)
+	}
+	expSlots := []slot{
+		{},
+		{},
+		{meetings: []meeting{m2, m1, m0}},
+		{meetings: []meeting{m3}},
+	}
+	if !reflect.DeepEqual(slots, expSlots) {
+		t.Error(slots, "final slots: expected", expSlots)
+	}
+}
+
+func TestRemoveSlotsIteration2(t *testing.T) {
+	m0 := meeting{index: 0, title: "Meeting 0", assistants: []string{"a", "e"}}
+	m1 := meeting{index: 1, title: "Meeting 1", assistants: []string{"b", "f"}}
+	m2 := meeting{index: 2, title: "Meeting 2", assistants: []string{"c", "g"}}
+	m3 := meeting{index: 3, title: "Meeting 3", assistants: []string{"d", "h"}}
+	m4 := meeting{index: 4, title: "Meeting 4", assistants: []string{"b", "c", "d"}}
+	m5 := meeting{index: 5, title: "Meeting 5", assistants: []string{"a", "c", "d"}}
+	m6 := meeting{index: 6, title: "Meeting 6", assistants: []string{"a", "b", "d"}}
+	m7 := meeting{index: 7, title: "Meeting 7", assistants: []string{"a", "b", "c"}}
+	mts := []meeting{m0, m1, m2, m3, m4, m5, m6, m7}
+	adjMat := calcAdjacencyMatrix(mts)
+	slots := make([]slot, len(mts))
+	for m_i, m := range mts {
+		slots[m_i] = slot{meetings: []meeting{m}}
+	}
+
+	i := 0
+	for slotsRemoved := true; slotsRemoved; i++ {
+		slotsRemoved = removeSlotsIteration(adjMat, &slots)
+	}
+	if expIt := 2; i != expIt {
+		t.Error(i, "iterations: expected", expIt)
+	}
+	expSlots := []slot{
+		{},
+		{},
+		{},
+		{},
+		{meetings: []meeting{m4, m0}},
+		{meetings: []meeting{m5, m1}},
+		{meetings: []meeting{m6, m2}},
+		{meetings: []meeting{m7, m3}},
+	}
+	if !reflect.DeepEqual(slots, expSlots) {
+		t.Error(slots, "final slots: expected", expSlots)
+	}
+}
+
+func TestFindMinimalSlots(t *testing.T) {
+	m0 := meeting{index: 0, title: "Meeting 0", assistants: []string{"a", "b"}}
+	m1 := meeting{index: 1, title: "Meeting 1", assistants: []string{"c", "d"}}
+	m2 := meeting{index: 2, title: "Meeting 2", assistants: []string{"e", "g"}}
+	m3 := meeting{index: 3, title: "Meeting 3", assistants: []string{"e", "h"}}
+	mts := []meeting{m0, m1, m2, m3}
+	slots, err := findMinimalSlots(mts)
+	if err != nil {
+		t.Error("unexpected error while calling findMinimalSlots:", err)
+	}
+	expSlots := []slot{
+		{meetings: []meeting{m2, m1, m0}},
+		{meetings: []meeting{m3}},
+	}
+	if !reflect.DeepEqual(slots, expSlots) {
+		t.Error(slots, "final slots: expected", expSlots)
+	}
+}
+
+func TestCreateMeetingsFromCSVData(t *testing.T) {
+	csvData := [][]string{
+		{"Meeting 0", "Maria Barker, John Garcia"},
+		{"Meeting 1", "Antonio Sanchez, Ana de Austria"},
+	}
+	meetings, err := createMeetingsFromCSVData(csvData)
+	if err != nil {
+		t.Error("unexpected error while calling createMeetingsFromCSVData:", err)
+	}
+	expMeets := []meeting{
+		{0, "Meeting 0", []string{"Maria Barker", "John Garcia"}, 0},
+		{1, "Meeting 1", []string{"Antonio Sanchez", "Ana de Austria"}, 0},
+	}
+	if !reflect.DeepEqual(meetings, expMeets) {
+		t.Error(meetings, "csv data read: expected", expMeets)
+	}
+}
+
+func TestMain(t *testing.T) {
+	out, err := ioutil.TempFile("", "slots*.csv")
+	if err != nil {
+		t.Error("cannot create temp file:", err)
+	}
+	defer os.Remove(out.Name())
+	out.Close()
+
+	os.Args = []string{"./main", "test-data/test1.csv", out.Name()}
+	main()
+	outData, err := os.ReadFile(out.Name())
+	if err != nil {
+		t.Error("cannot read result from processing of test1:", err)
+	}
+	expected, err := os.ReadFile("test-data/res1.csv")
+	if err != nil {
+		t.Error("cannot read res1.csv:", err)
+	}
+	if bytes.Compare(outData, expected) != 0 {
+		t.Errorf("expected:\n%q\n, obtained:\n%q\n", string(expected), string(outData))
+	}
+}
