76cb02e84b608e71a2e04b01aa245e0784e4bca4
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-07-24 11:18:30 +0100

    main: some imp

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ type slot struct {
 // Input must be in the form of a csv file, with the first column being
 // the meeting title and the second the list of assistants.
 // Output will be a csv file with two columns, one with the slot number
-// and the second with the simutaneous meetings happening in that slot.
+// and the second with the simultaneous meetings happening in that slot.
 // Steps:
 // 1. Read meetings from input
 // 2. Calculate adjacency matrix
@@ -51,20 +51,31 @@ func main() {
 		fmt.Printf("Error reading csv data from %s: %v\n", os.Args[1], err)
 		os.Exit(1)
 	}
-	meetings, err := createMeetingsFromCVSData(csvData)
+	meetings, err := createMeetingsFromCSVData(csvData)
 	if err != nil {
 		fmt.Printf("Error reading csv meeting data from %s: %v\n", os.Args[1], err)
 		os.Exit(1)
 	}
 	slots, err := findMinimalSlots(meetings)
+
+	csvWriter := csv.NewWriter(os.Stdout)
 	for sl_i, sl := range slots {
 		for _, mt := range sl.meetings {
-			fmt.Printf("Slot %d, %q, %q\n", sl_i, mt.title, mt.assistants)
+			assistants := ""
+			for i := 0; i < len(mt.assistants)-1; i++ {
+				assistants += mt.assistants[i]
+				assistants += ", "
+			}
+			assistants += mt.assistants[len(mt.assistants)-1]
+			csvWriter.Write([]string{fmt.Sprintf("slot %d", sl_i),
+				mt.title,
+				assistants})
 		}
 	}
+	csvWriter.Flush()
 }
 
-func createMeetingsFromCVSData(csvData [][]string) ([]meeting, error) {
+func createMeetingsFromCSVData(csvData [][]string) ([]meeting, error) {
 	meetings := make([]meeting, len(csvData))
 	for i, line := range csvData {
 		if len(line) != 2 {
@@ -75,7 +86,7 @@ func createMeetingsFromCVSData(csvData [][]string) ([]meeting, error) {
 		for _, person := range assistants {
 			mt.assistants = append(mt.assistants, strings.TrimSpace(person))
 		}
-		meetings = append(meetings, mt)
+		meetings[i] = mt
 	}
 
 	return meetings, nil
@@ -96,7 +107,15 @@ func findMinimalSlots(meetings []meeting) ([]slot, error) {
 	for removed := true; removed == true; removed = removeSlotsIteration(adj, &slots) {
 	}
 
-	return slots, nil
+	// Remove emptied slots
+	var consolidatedSlots []slot
+	for _, sl := range slots {
+		if len(sl.meetings) != 0 {
+			consolidatedSlots = append(consolidatedSlots, sl)
+		}
+	}
+
+	return consolidatedSlots, nil
 }
 
 // Implements the removal strategy, one iteration across current slots.
